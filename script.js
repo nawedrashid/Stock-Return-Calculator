498e5621fe7369e11218e2a015f60cf353eb98ee
nawedrashid <rashidnawed@gmail.com>
2021-09-30 03:17:33 +0530

    added html css and js

---
+++ script.js
@@ -0,0 +1,27 @@
+var InitialPrice = document.querySelector("#initial-price");
+var StocksQuantity = document.querySelector("#stocks-quantity");
+var CurrentPrice = document.querySelector("#current-price");
+var CheckBtn = document.querySelector("#check-btn");
+var Output = document.querySelector("#output");
+
+CheckBtn.addEventListener("click", function CalculateProfitAndLoss(){
+    Output.style.display="none";
+    var initial = InitialPrice.value;
+    var quantity = StocksQuantity.value;
+    var current = CurrentPrice.value;
+    if(initial>current){
+        var loss = (initial - current)* quantity;
+        var lossPercentage = (loss/initial)*100;
+        showMessage(`The loss is ${loss} The loss percent is ${lossPercentage}%`);
+    } else if(initial<current){
+        var profit = (current - initial)* quantity;
+        var ProfitPercentage = (profit/initial)*100;
+        showMessage(`The Profit is ${profit} and The Profit percent is ${ProfitPercentage}%`);
+    } else {
+        showMessage("Welcome to the static world")
+    }
+});
+function showMessage(msg){
+    Output.innerText = msg;
+    Output.style.display = "block";
+}
